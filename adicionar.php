3abaf67b85681a791c6c906eb09f307feba83e36
EverthonH <Everthon.henrique.75@gmail.com>
2020-03-10 02:27:13 -0300

    Atualização do css do login

--- adicionar.php
+++ adicionar.php
@@ -3,44 +3,37 @@
 <?php
 $nome = $_POST['nome'];
 $email = $_POST['email'];
-$senha = $_POST['senha'];
 $validaremail = explode('@', $email);
 $errodepreenchimento = 0;
 $errodeemail = 0;
-$errodenome = 0;
 ?>
-
+<!-- <?php
+// include('paginainicial.html');
+?> -->
 <?php if (empty($nome) || empty($email)): ?>
    <?php $errodepreenchimento = 1; ?>
-   <?php include 'paginainicial.html' ?>
-   <div id="erro">Preencha todos os campos!</div>
+
+   
 <?php endif ?>
 
 <?php if ($validaremail[1] != 'discente.ifpe.edu.br' && $validaremail[1] != 'igarassu.ifpe.edu.br'): ?>
   <?php $errodeemail = 1; ?>
 <?php endif ?>
 
-<?php if (preg_match("/([a-zA-ZÁ-Úá-ú ]+)/", $nome) == false && $errodepreenchimento == 0 && $errodeemail == 0): ?>
-  <?php $errodenome = 1; ?>
-  <?php include 'paginainicial.html' ?>
-  <div id="erro">Nenhum caractere especial ou número pode ser usado no campo de nome!</div>
-<?php endif ?>
-
-<?php if ($errodeemail == 1 && $errodepreenchimento == 0 && $errodenome == 0): ?> 
+<?php if ($errodeemail == 1 && $errodepreenchimento == 0): ?> 
    <?php include 'paginainicial.html' ?>
-   <div id="erro">É necessário que você possua um e-mail institucional!</div>
+   <div id="erro"><p>É necessário que você possua um e-mail institucional!</p></div>
 <?php endif ?>
 
 <?php if ($validaremail[1] == 'discente.ifpe.edu.br' || $validaremail[1] == 'igarassu.ifpe.edu.br'): ?>
   <?php $errodeemail = 0; ?>
 <?php endif ?>
 
-<?php if ($errodepreenchimento == 0 && $errodeemail == 0 && $errodenome == 0): ?>
+<?php if ($errodepreenchimento == 0 && $errodeemail == 0): ?>
    <h3>Confirmar conta:</h3>
    <ul>
         <li>Nome: <strong><?= $nome ?></strong></li>
         <li>E-mail: <strong><?= $email ?></strong></li>
-        <li>Senha: <strong><?= $senha ?></strong></li>
         <form>
           <fieldset>
               <legend>Digite o código enviado para o seu e-mail:</legend>
@@ -49,4 +42,5 @@ $errodenome = 0;
             </fieldset>
         </form>
    </ul>
+   <a href="/">Voltar</a>
 <?php endif ?>
