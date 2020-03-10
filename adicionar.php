d90ec233d7769f8e799580bef66c29bfb7fa2b65
FabioMouradeFraga <fabiomouradefraga@gmail.com>
2020-03-09 21:57:45 -0300

    Correção do Sistema de Autenticação

--- adicionar.php
+++ adicionar.php
@@ -4,38 +4,39 @@
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $validaremail = explode('@', $email);
-$erro = 0;
+$errodepreenchimento = 0;
+$errodeemail = 0;
 ?>
 
-<?php if ($validaremail[1] == 'discente.ifpe.edu.br' || $validaremail[1] == 'igarassu.ifpe.edu.br'): ?>
-	<?php $erro = 0; ?>
-<?php endif ?>
-
 <?php if (empty($nome) || empty($email)): ?>
-   <?php $erro = 1; ?>
+   <?php $errodepreenchimento = 1; ?>
    <?php include 'paginainicial.html' ?>
    <div id="erro">Preencha todos os campos!</div>
 <?php endif ?>
 
-<?php if ($validaremail[1] != 'discente.ifpe.edu.br' && $validaremail[1] != 'igarassu.ifpe.edu.br'	): ?>
-	<?php $erro = 1; ?>
+<?php if ($validaremail[1] != 'discente.ifpe.edu.br' && $validaremail[1] != 'igarassu.ifpe.edu.br'): ?>
+  <?php $errodeemail = 1; ?>
 <?php endif ?>
 
-<?php if ($erro == 1): ?> 
+<?php if ($errodeemail == 1 && $errodepreenchimento == 0): ?> 
    <?php include 'paginainicial.html' ?>
    <div id="erro">É necessário que você possua um e-mail institucional!</div>
 <?php endif ?>
 
-<?php if ($erro == 0): ?>
+<?php if ($validaremail[1] == 'discente.ifpe.edu.br' || $validaremail[1] == 'igarassu.ifpe.edu.br'): ?>
+  <?php $errodeemail = 0; ?>
+<?php endif ?>
+
+<?php if ($errodepreenchimento == 0 && $errodeemail == 0): ?>
    <h3>Confirmar conta:</h3>
    <ul>
         <li>Nome: <strong><?= $nome ?></strong></li>
         <li>E-mail: <strong><?= $email ?></strong></li>
         <form>
-        	<fieldset>
-    	        <legend>Digite o código enviado para o seu e-mail:</legend>
+          <fieldset>
+              <legend>Digite o código enviado para o seu e-mail:</legend>
                 <input type="text" name="codigo" placeholder="Código">
-                <input type="submit" value="Confirmar">	
+                <input type="submit" value="Confirmar"> 
             </fieldset>
         </form>
    </ul>
