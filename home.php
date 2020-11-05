7a6915d8aef92c67ad6f979b038947e1a2dbf5d5
DaniloM2 <danilo280102@gmail.com>
2020-11-05 01:43:52 -0300

    Exclusão de consulta desnecessaria ao banco

--- home.php
+++ home.php
@@ -20,24 +20,6 @@ include 'bd.php';
 <body>
 
 	<main>
-
-		<?php 
-
-		$stmt = $pdo->prepare("
-		SELECT * FROM USERS
-		WHERE US_ID = ?
-		");
-
-		$stmt->execute([$_SESSION['login'],]);
-
-		$linhas = $stmt->fetchAll();
-
-		$_SESSION['name'] = $linhas[0]['US_NAME'];
-
-		$user = $_SESSION['name'];
-
-		?>
-
 		<?php if (!isset($_SESSION['login'])): ?>
 			<h1 class="text-center mb-4"><strong>Olá! Confira os últimos tópicos! :)</strong></h1>
 		<?php endif ?>
@@ -101,7 +83,7 @@ include 'bd.php';
   				<div class="col-2 pt-3">
 
   					<?php if ($consulta[$i]['US_IMAGE'] == null || $consulta[$i]['US_IMAGE'] == 'upload/'): ?>
-                    	<svg width="3em" height="3em" viewBox="0 0 20 20" class="bi bi-person-circle d-block m-auto" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    	<svg width="3em" height="3em" viewBox="0 0 20 20" class="bi bi-person-circle d-block m-auto" fill	="currentColor" xmlns="http://www.w3.org/2000/svg">
   							<path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
   							<path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
   							<path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
@@ -114,11 +96,15 @@ include 'bd.php';
     			</div>
     			<div class="col-3 text-left pt-3">Criado <time class="timeago" datetime="<?= date('Y-m-d H:m:s', strtotime($consulta[$i]['TOP_DATE'])) ?>"></time></div>
     			<div class="col-2">
-    				<a href="#">
-    					<svg width="3em" height="3em" viewBox="0 0 20 20" class="bi bi-trash-fill d-block m-auto pt-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
- 							<path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
-						</svg>
-					</a>
+    				<?php if (isset($_SESSION['login'])): ?>
+    					<?php if ($consulta[$i]['TOP_US_ID'] == $_SESSION['login']): ?>
+    						<a href="delete.php?id=<?=$consulta[$i]['TOP_ID'] ?>">
+    							<svg width="3em" height="3em" viewBox="0 0 20 20" class="bi bi-trash-fill d-block m-auto pt-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+ 								<path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+								</svg>
+    						</a>	
+    					<?php endif ?>
+    				<?php endif ?>
     			</div>
   			</div>
   			<div class="row justify-content-center">
