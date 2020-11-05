15e3adcc7c49051c014e6c2114f1b444d647cf5d
DaniloM2 <danilo280102@gmail.com>
2020-11-05 17:08:18 -0300

    Alert de confirmação de exclusão de tópico

--- home.php
+++ home.php
@@ -101,7 +101,7 @@ include 'bd.php';
     					</div>
     						<?php if (isset($_SESSION['login']) && $dados[$i]['TOP_US_ID'] == $_SESSION['login']): ?>
     							<div class="col-2">
-    								<a href="delete.php?id=<?= $dados[$i]['TOP_ID'] ?>">
+    								<a class="delete" href="delete.php?id=<?= $dados[$i]['TOP_ID'] ?>">
     									<svg width="3em" height="3em" viewBox="0 0 20 20" class="bi bi-trash-fill d-block m-auto pt-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
  											<path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
 										</svg>
@@ -126,7 +126,11 @@ include 'bd.php';
   					</div>
 				</div>
 			<?php endfor ?>
-
+			<script>
+				$('.delete').on('click', evt => {
+					if (!confirm("Quer realmente apagar o seu Tópico?")) evt.preventDefault() 
+				})
+			</script>	
 		</main>
 
 		<?php 
