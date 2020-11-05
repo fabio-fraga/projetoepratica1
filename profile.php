c99537002556bb7bb805f8f0f9fa3abf972472c3
FabioMouradeFraga <fabiomouradefraga.com>
2020-11-05 15:37:28 -0300

    Implementação do serviço de validação

--- profile.php
+++ profile.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Perfil</title>
-  
-<?php
+  <meta charset="UTF-8">
+  <title>Editar Perfil</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-include 'templates/header.php';
+  <?php 
+  include 'templates/header.php';
+  ?>
+
+</head>
+
+<?php 
+
 include 'bd.php';
 
 $stmt = $pdo->prepare("
@@ -20,41 +26,49 @@ $consulta = $stmt->fetchAll();
 
 ?>
 
-  <div id="div-principal-profile">
+<body>
+  <main>
+    
+  <div">
 
-    <div id="div-form-profile">
+    <div">
 
-      <?php for ($i = 0; $i < sizeof($consulta); $i++): ?>
-        <?php if($_SESSION['login'] == $consulta[$i]['US_ID']): ?>
+        <?php if($_SESSION['login'] == $consulta[0]['US_ID']): ?>
 
           <form enctype="multipart/form-data" method="POST" action="profile_edit.php">
-              <h1 id="titulo-profile">Editar Informações</h1>
+              <h1>Editar Informações</h1>
 
-              <?php if ($consulta[$i]['US_IMAGE'] == null): ?>                
-                <img id="image-profile" src="upload/standard.png">
-                <input id="input-image" type="file" name="image">
+              <?php if ($consulta[0]['US_IMAGE'] == null): ?>                
+                <svg width="3em" height="3em" viewBox="0 0 20 20" class="bi bi-person-circle d-block m-auto" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
+                  <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                  <path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
+                </svg>
               <?php else: ?>
-                <img id="image-profile" src="<?= $consulta[$i]['US_IMAGE'] ?>">
-                <input id="input-image" type="file" name="image">
+                <img src="<?= $consulta[0]['US_IMAGE'] ?>">
               <?php endif ?>
-
-              <input class="inputs-form-profile" type="text" name="name" placeholder="Nome"value="<?= $consulta[$i]['US_NAME'] ?>">
-              <input class="inputs-form-profile" type="text" name="linkedin" placeholder="LinkedIn"value="<?= $consulta[$i]['US_LINKEDIN'] ?>">
-              <input class="inputs-form-profile" type="text" name="github" placeholder="GitHub"value="<?= $consulta[$i]['US_GITHUB'] ?>">
-              <input class="inputs-form-profile" type="date" name="birth" placeholder="Nascimento"value="<?= $consulta[$i]['US_BIRTH'] ?>">
-              <input class="inputs-form-profile" type="text" name="description" placeholder="Descrição"value="<?= $consulta[$i]['US_DESCRIPTION'] ?>">
-              <input class="inputs-form-profile" type="submit" value="Alterar">
+              <input type="text" name="linkedin" placeholder="LinkedIn"value="<?= $consulta[0]['US_LINKEDIN'] ?>">
+              <input type="text" name="github" placeholder="GitHub"value="<?= $consulta[0]['US_GITHUB'] ?>">
+              <input type="date" name="birth" placeholder="Nascimento"value="<?= $consulta[0]['US_BIRTH'] ?>">
+              <input type="text" name="description" placeholder="Descrição"value="<?= $consulta[0]['US_DESCRIPTION'] ?>">
+              <p><?= $report_erro ?></p>
+              <input type="file" name="image">
+              <input type="submit" value="Alterar">
           </form>
 
         <?php endif ?>
-      <?php endfor?>
 
     </div>
 
   </div>
 
+  </main>
 <?php
 
 include 'templates/footer.php';
 
 ?>
+
+</body>
+
+</html>
--- profile_edit.php
+++ profile_edit.php
@@ -4,34 +4,81 @@ session_start();
 
 include 'bd.php';
 
-$uploaddir = 'upload/';
-$uploadfile = $uploaddir . $_FILES['image']['name'];
-
-$nome = $_POST['name'];
-$linkedin = $_POST['linkedin']; 
+$linkedin = $_POST['linkedin'];
 $github = $_POST['github'];
-$nascimento = $_POST['birth'];
-$descricao = $_POST['description'];
+$birth = $_POST['birth'];
+$description = $_POST['description'];
 
-move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile);
+$erro_campos = false;
+$report_erro = '';
 
-$stmt = $pdo->prepare("
-  UPDATE USERS
-  SET
-  US_NAME = ?,
-  US_LINKEDIN = ?,
-  US_GITHUB = ?,
-  US_BIRTH = ?,
-  US_DESCRIPTION = ?,
-  US_IMAGE = ?
-  WHERE US_ID = ?
-");
+function empty_text ($string) {
+  $array = str_split($string);
+  $cont = 0;
+  for ($i = 0; $i < sizeof($array); $i++) {
+    if ($array[$i] == ' ') {
+      $cont++;
+    }
+  }
+  if ($cont == sizeof($array)) {
+    return true;
+  }
+  return false;
+}
+
+if (empty($birth) || empty_text($birth)) {
+  $erro_campos = true;
+  $report_erro = 'Preencha todos os campos obrigatórios!';  
+  include 'profile.php';
+}
+
+if ($erro_campos == false) {
+
+if (isset($_FILES['image']) && is_uploaded_file($_FILES["image"]["tmp_name"])) {
+
+    if (!file_exists('upload/' . $_SESSION['login'])) { 
+      mkdir('upload/' . $_SESSION['login'], 0700, true);
+    }
+    if (strtolower(substr($_FILES['image']['name'], -4)) != '.jpg' && strtolower(substr($_FILES['image']['name'], -4)) != '.png') {
+      $extensao = '.jpg';
+    } else {
+      $extensao = strtolower(substr($_FILES['image']['name'], -4));
+    }
+
+    $new_name = md5(time()) . $extensao;
+
+    $img = 'upload/' . $_SESSION['login'] . '/' . $new_name;
+
+    move_uploaded_file($_FILES['image']['tmp_name'], $img);
+
+	$stmt = $pdo->prepare("
+  		UPDATE USERS
+  		SET
+  		US_LINKEDIN = ?,
+  		US_GITHUB = ?,
+  		US_BIRTH = ?,
+  		US_DESCRIPTION = ?,
+  		US_IMAGE = ?
+  		WHERE US_ID = ?
+	");
+
+	$stmt->execute([$linkedin, $github, $birth, $description, $img, $_SESSION['login']]);
+
+} else {
+	$stmt = $pdo->prepare("
+  		UPDATE USERS
+  		SET
+  		US_LINKEDIN = ?,
+  		US_GITHUB = ?,
+  		US_BIRTH = ?,
+  		US_DESCRIPTION = ?,
+  		WHERE US_ID = ?
+	");
 
 
-$stmt->execute([$nome, $linkedin, $github, $nascimento, $descricao, $uploadfile, $_SESSION['login']]);
-
-unset($_FILES);
-
-header('location: profile.php');
-
+	$stmt->execute([$linkedin, $github, $birth, $description, $_SESSION['login']]);
+}
+	unset($_FILES);
+	header('location: profile.php');
+}
 ?>
