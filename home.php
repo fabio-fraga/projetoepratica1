7d02c6b29c1e3e5a7f895d91dd73a5dc08146784
FabioMouradeFraga <fabiomouradefraga.com>
2020-11-05 15:35:20 -0300

    Alterando CSS e implementando validação de envio de dados

--- home.php
+++ home.php
@@ -1,7 +1,5 @@
 <?php 
 
-session_start();
-
 include 'bd.php';
 
 ?>
@@ -13,117 +11,127 @@ include 'bd.php';
 	<title>Bem-vindo ao F4ALL!</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<?php include 'templates/header.php'; ?>
+	<?php 
+	include 'templates/header.php';
+	?>
 
 </head>
 
-<body>
+	<body>
 
-	<main>
-		<?php if (!isset($_SESSION['login'])): ?>
-			<h1 class="text-center mb-4"><strong>Olá! Confira os últimos tópicos! :)</strong></h1>
-		<?php endif ?>
+		<main>
 
-		<?php if (isset($_SESSION['login'])): ?>
-				<div class="container mb-3 shadow border border-light rounded">
-					<form method="POST" action="topics.php"class="text-center mt-n3 mb-3">
-						<div class="row justify-content-center ml-4 mt-5">
+			<?php if (!isset($_SESSION['login'])): ?>
+				<h1 class="text-center mb-4">
+					<strong>Olá! Confira os últimos tópicos! :)</strong>
+				</h1>
+			<?php endif ?>
 
-							<div class="col-10 text-left ml-n5 pb-3">
-								<div class="row justify-content-center ml-4">
-									<h2 class=" text-center"><strong>O que deseja saber?</strong></h2>
-								</div>
+			<?php if (isset($_SESSION['login'])): ?>
+				<div class="container mt-n3 mb-3 shadow border border-light rounded">
+					<form enctype="multipart/form-data" method="POST" action="topics.php" class="text-center mt-n3 mb-3">
+						<div class="row justify-content-center mt-5">
+							<div class="col-12 pb-3">
+								<h2 class=" text-center">
+									<strong>O que deseja saber?</strong>
+								</h2>
 							</div>
-
-							<div class="col-10 text-left ml-n5 pb-3">
-								<div class="row justify-content-center ml-4">
-									<textarea type="text" class="form-control" name="title" placeholder="Dê um título ao tópico:" style="resize: none"></textarea>
-								</div>
+						</div>
+						<div class="row justify-content-center">
+							<div class="col-11 text-left pb-3">
+								<textarea type="text" class="form-control" name="title" placeholder="Dê um título ao tópico:" style="resize: none"><?= $_POST['title'] ?? '' ?></textarea>
+								<p class="text-danger text-left">(Campo obrigatório)</p>
 							</div>
-
-								<div class="col-10 text-left ml-n5 pb-3">
-							<div class="row justify-content-center ml-4">
-								<textarea type="text" class="form-control" name="subject" placeholder="Descreva seu problema ou dúvida:" style="resize: none"></textarea>
-								</div>
-
+						</div>
+						<div class="row justify-content-center mt-n3">
+							<div class="col-11 text-left pb-3">
+								<textarea type="text" class="form-control" name="subject" placeholder="Descreva seu problema ou dúvida:" style="resize: none"><?= $_POST['subject'] ?? '' ?></textarea>
+								<p class="text-danger text-left">(Campo obrigatório)</p>
 							</div>
-							<div class="col-10 text-left ml-n5 pb-3">
-								<div class="row justify-content-center ml-4 mb-2">
-									<input type="submit" value="Publicar" class="btn btn-primary">
-								</div>
-
+						</div>
+						<div class="row justify-content-center mt-n3">
+							<div class="col-11 text-center pb-3">
+								<input class="form-control-file" type="file" name="img" accept="image/jpeg, image/png">
 							</div>
-
+						</div>
+						<div class="row justify-content-center">
+							<div class="col-11 text-center mt-n2 pb-3">
+								<p class="text-danger"><strong><?= $report_erro ?></strong></p>
+							</div>
+						</div>
+						<div class="row justify-content-center">
+							<div class="col-11 text-center mt-n4 pb-3">
+								<input type="submit" value="Publicar" class="btn btn-primary">
+							</div>
 						</div>
 					</form>
 				</div>
+			<?php endif ?>
 
-		<?php endif ?>
+			<?php
+			$stmt = $pdo->prepare("
+				SELECT *
+          		FROM TOPICS
+          		JOIN USERS ON TOP_US_ID = US_ID
+          		ORDER BY TOP_DATE DESC
+			");
 
-		<?php
+			$stmt->execute();
+			$dados = $stmt->fetchAll();
+			?>
 
-		$stmt = $pdo->prepare("
-		  SELECT *
-          FROM TOPICS
-          JOIN USERS ON TOP_US_ID = US_ID
-          ORDER BY TOP_DATE DESC
-		");
+			<?php for ($i = 0; $i < sizeof($dados); $i++): ?>
+ 				<div class="container mb-3 shadow border border-light rounded">
+  					<div class="row justify-content-center">
+  						<div class="col-2 pt-3">
+  							<?php if ($dados[$i]['US_IMAGE'] == null || $dados[$i]['US_IMAGE'] == 'upload/' . $_SESSION['login']): ?>
+                    			<svg width="3em" height="3em" viewBox="0 0 20 20" class="bi bi-person-circle d-block m-auto" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  									<path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
+  									<path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+  									<path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
+								</svg>
+                    		<?php else: ?>
+                    			<img class="img-topics rounded-circle d-block m-auto" src="<?= $dados[$i]['US_IMAGE'] ?>">
+             				<?php endif ?>
+  						</div>
+    					<div class="col-5 text-left pt-3">
+    						<strong><?= $dados[$i]['TOP_TITLE'] ?></strong>
+    					</div>
+    					<div class="col-3 text-right pt-3">Criado <time class="timeago" datetime="<?= date('Y-m-d H:m:s', strtotime($dados[$i]['TOP_DATE'])) ?>"></time>
+    					</div>
+    						<?php if (isset($_SESSION['login']) && $dados[$i]['TOP_US_ID'] == $_SESSION['login']): ?>
+    							<div class="col-2">
+    								<a href="delete.php?id=<?= $dados[$i]['TOP_ID'] ?>">
+    									<svg width="3em" height="3em" viewBox="0 0 20 20" class="bi bi-trash-fill d-block m-auto pt-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+ 											<path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+										</svg>
+									</a>
+    							</div>
+    						<?php else: ?>
+    							<div class="col-2"></div>
+    						<?php endif ?>
+  					</div>
+  					<div class="row justify-content-center">
+    					<div class="col-2 text-center ml-n2">Criado por: <?= $dados[$i]['US_NAME'] ?></div>
+    					<div class="col-10"></div>
+  					</div>
+  					<div class="row justify-content-center pb-3">
+	  					<div class="col-2"></div>
+    					<div class="col-8 text-center">
+    						<a class="a-topics" href="discussao.php?id=<?=$dados[$i]['TOP_ID'] ?>">
+    							<strong>Clique aqui para visualizar toda a discussão!</strong>
+    						</a>
+    					</div>
+    					<div class="col-2"></div>
+  					</div>
+				</div>
+			<?php endfor ?>
 
-		$stmt->execute();
-		$consulta = $stmt->fetchAll();
+		</main>
 
+		<?php 
+		include 'templates/footer.php';
 		?>
 
-		<?php for ($i = 0; $i < sizeof($consulta); $i++): ?>
-
-
- 		<div class="container mb-3 shadow border border-light rounded">
-
-  			<div class="row justify-content-center">
-  				<div class="col-2 pt-3">
-
-  					<?php if ($consulta[$i]['US_IMAGE'] == null || $consulta[$i]['US_IMAGE'] == 'upload/'): ?>
-                    	<svg width="3em" height="3em" viewBox="0 0 20 20" class="bi bi-person-circle d-block m-auto" fill	="currentColor" xmlns="http://www.w3.org/2000/svg">
-  							<path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
-  							<path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-  							<path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
-						</svg>
-                    <?php else: ?>
-                    		<img class="img-topics rounded d-block m-auto" src="<?= $consulta[$i]['US_IMAGE'] ?>">
-             <?php endif ?>
-  				</div>
-    			<div class="col-5 text-left pt-3"><strong><?=$consulta[$i]['TOP_TITLE'] ?></strong>
-    			</div>
-    			<div class="col-3 text-left pt-3">Criado <time class="timeago" datetime="<?= date('Y-m-d H:m:s', strtotime($consulta[$i]['TOP_DATE'])) ?>"></time></div>
-    			<div class="col-2">
-    				<?php if (isset($_SESSION['login'])): ?>
-    					<?php if ($consulta[$i]['TOP_US_ID'] == $_SESSION['login']): ?>
-    						<a href="delete.php?id=<?=$consulta[$i]['TOP_ID'] ?>">
-    							<svg width="3em" height="3em" viewBox="0 0 20 20" class="bi bi-trash-fill d-block m-auto pt-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
- 								<path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
-								</svg>
-    						</a>	
-    					<?php endif ?>
-    				<?php endif ?>
-    			</div>
-  			</div>
-  			<div class="row justify-content-center">
-    			<div class="col-2 text-center ml-n2">Criado por: <?= $consulta[$i]['US_NAME'] ?></div>
-    			<div class="col-10"></div>
-  			</div>
-  			<div class="row justify-content-center pb-3">
-	  			<div class="col-10"></div>
-    			<div class="col-2 text-center"><a class="a-topics" href="discussao.php?id=<?=$consulta[$i]['TOP_ID'] ?>"><strong>Ver mais...</strong></a></div>
-  			</div>
-
-		</div>
-
-		<?php endfor ?>
-
-	</main>
-
-	<?php include 'templates/footer.php'; ?>
-
-</body>
-
+	</body>
 </html>
--- topics.php
+++ topics.php
@@ -1,24 +1,101 @@
 <?php
+
 session_start();
+
 include 'bd.php';
 
-// $_SESSION['id'] = $linhas[0]['US_ID'];
-
-$subject = nl2br(htmlentities( $_POST['subject']));
+$subject = nl2br(htmlentities($_POST['subject']));
 $title = htmlentities($_POST['title']);
 $date = date("Y-m-d H:i:s");
+$date_save = $date;
 $user = $_SESSION['login'];
+$img_nome = $_FILES['img']['name'];
 
+$erro_campos = false;
+$erro_caracteres_title = false;
+$erro_type_img = false;
+$report_erro = '';
 
- $stmt = $pdo->prepare("
-    INSERT INTO TOPICS( TOP_SUBJECT, TOP_TITLE, TOP_DATE, TOP_US_ID)
-VALUES (?,?,?,?)
+function empty_text ($string) {
+  $array = str_split($string);
+  $cont = 0;
+  for ($i = 0; $i < sizeof($array); $i++) {
+    if ($array[$i] == ' ') {
+      $cont++;
+    }
+  }
+  if ($cont == sizeof($array)) {
+    return true;
+  }
+  return false;
+}
+
+if (empty($title) || empty_text($title) || empty($subject) || empty_text($subject)) {
+	$erro_campos = true;
+	$report_erro = 'Preencha todos os campos obrigatórios!'; 
+  include 'home.php';
+}
+
+if (strlen($title) > 255 && $erro_campos == false) {
+	$erro_caracteres_title = true;
+	$report_erro = 'O título do tópico não pode conter mais de 255 caracteres!';
+	include 'home.php';
+}
+
+if (substr($img_nome, -3) == 'png' && $erro_campos == false && $erro_caracteres_title == false) {
+  $erro_type_img = false;
+} elseif (substr($img_nome, -3) == 'jpg' && $erro_campos == false && $erro_caracteres_title == false) {
+  $erro_type_img = false; 
+} elseif (substr($img_nome, -4) == 'jpeg' && $erro_campos == false && $erro_caracteres_title == false) {
+  $erro_type_img = false;
+} elseif (substr($img_nome, -4) == '' && $erro_campos == false && $erro_caracteres_title == false) {
+  $erro_type_img = false;
+} else {
+  $erro_type_img = true; 
+  $report_erro = 'Formatos de imagens diferentes de PNG, JPG e JPEG não são permitidos!';
+  include 'home.php';
+}
+
+if ($erro_campos == false && $erro_caracteres_title == false && $erro_type_img == false) {
+  if (isset($_FILES['img'])) {
+
+    if (!file_exists('upload/' . $_SESSION['login'])) { 
+      mkdir('upload/' . $_SESSION['login'], 0700, true);
+    }
+    if (strtolower(substr($_FILES['img']['name'], -4)) != '.jpg' && strtolower(substr($_FILES['img']['name'], -4)) != '.png') {
+      $extensao = '.jpg';
+    } else {
+      $extensao = strtolower(substr($_FILES['img']['name'], -4));
+    }
+
+    $new_name = md5(time()) . $extensao;
+
+    $img = 'upload/' . $_SESSION['login'] . '/' . $new_name;
+
+    move_uploaded_file($_FILES['img']['tmp_name'], $img);
+
+    $stmt = $pdo->prepare("
+		  INSERT INTO TOPICS(TOP_SUBJECT, TOP_TITLE, TOP_DATE, TOP_US_ID, TOP_IMAGE)
+		  VALUES (?,?,?,?,?)
     ");
 
-$stmt->execute([$subject, $title, $date, $user]);
+    $stmt->execute([$subject, $title, $date, $user, $img]);
 
+  } else {
+    $stmt = $pdo->prepare("
+      INSERT INTO TOPICS(TOP_SUBJECT, TOP_TITLE, TOP_DATE, TOP_US_ID)
+      VALUES (?,?,?,?)
+    ");
 
-header('location: home.php');
+    $stmt->execute([$subject, $title, $date, $user]);
 
+  }
 
- ?>
+  unset($_POST);
+  unset($_FILES);
+
+  header('location: home.php');
+
+}
+
+?>
