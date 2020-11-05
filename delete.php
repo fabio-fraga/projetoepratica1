2691ccb8da0ac1f9acd14bc8e69e1db729f09295
DaniloM2 <danilo280102@gmail.com>
2020-11-05 12:08:31 -0300

    Alteração no código para apagar somente os comentarios pertecente ao topico

--- delete.php
+++ delete.php
@@ -4,12 +4,18 @@
 
 	session_start();
 
-	$tp = $_GET['id'];
+	$erase = $_GET['id'];
+
+    $stmt = $pdo->prepare("DELETE FROM COMMENTS WHERE COM_TOP_ID = ?");
+
+    $stmt->execute([$erase]);
+
+ ?>
+<?php 
 
     $stmt = $pdo->prepare("DELETE FROM TOPICS WHERE TOP_ID = ?");
 
-    $stmt->execute([$tp]);
+    $stmt->execute([$erase]);
 
     header('location:home.php');
-
- ?>
+?>
