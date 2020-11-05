688faaa0e9f26d6c179818a8121a69d269ef7f21
DaniloM2 <danilo280102@gmail.com>
2020-11-05 16:50:06 -0300

    Mudança no código possibilitando apagar os likes e deslikes

--- delete.php
+++ delete.php
@@ -13,9 +13,18 @@
  ?>
 <?php 
 
+	$stmt = $pdo->prepare("DELETE FROM VOTE WHERE VOTE_TOP_ID = ?");
+
+    $stmt->execute([$erase]);
+
+?>
+<?php 
+
+
     $stmt = $pdo->prepare("DELETE FROM TOPICS WHERE TOP_ID = ?");
 
     $stmt->execute([$erase]);
 
     header('location:home.php');
+
 ?>
