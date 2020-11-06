2511702736fdd4716825660060001cae14909787
DaniloM2 <danilo280102@gmail.com>
2020-11-06 13:04:58 -0300

    Correção de bugs e melhorias no codigo de like/dislike

--- rating.php
+++ rating.php
@@ -14,13 +14,14 @@ $linhas = $stmt->fetchAll();
 
 	if ($linhas[0]['VOTE_VALUE'] == null) {
 
-		if ($_GET['valor'] == 1){
+		if ($_GET['valor'] == 1) {
 		
 			$stmt = $pdo->prepare("INSERT INTO VOTE (VOTE_US_ID, VOTE_TOP_ID, VOTE_VALUE) VALUES (?,?, true)");
 
 			$stmt->execute([$_SESSION['login'], $ld]);	
+	
 	}
-		if ($_GET['valor'] == 0){
+		if ($_GET['valor'] == 0) {
 		
 			$stmt = $pdo->prepare("INSERT INTO VOTE (VOTE_US_ID, VOTE_TOP_ID, VOTE_VALUE) VALUES (?,?, false)");
 
@@ -29,10 +30,27 @@ $linhas = $stmt->fetchAll();
 }
 	else{
 
-		$stmt = $pdo->prepare("DELETE FROM VOTE WHERE VOTE_TOP_ID = ?");
+		if ($_GET['valor'] == 0 && $linhas[0]['VOTE_VALUE'] == 1) {
 
-		$stmt->execute([$ld]);
+			$stmt = $pdo->prepare("UPDATE VOTE SET VOTE_VALUE = false WHERE VOTE_US_ID = ? AND VOTE_TOP_ID = ?");
 
+			$stmt->execute([$_SESSION['login'], $ld]);		
+			
+		}
+		elseif ($_GET['valor'] == 1 && $linhas[0]['VOTE_VALUE'] == 0) {
+
+			$stmt = $pdo->prepare("UPDATE VOTE SET VOTE_VALUE = true WHERE VOTE_US_ID = ? AND VOTE_TOP_ID = ?");
+
+			$stmt->execute([$_SESSION['login'], $ld]);		
+			
+		}
+		else{	
+
+		$stmt = $pdo->prepare("DELETE FROM VOTE WHERE VOTE_US_ID = ? AND VOTE_TOP_ID = ?");
+
+		$stmt->execute([$_SESSION['login'], $ld]);
+		
+		}
 }
 header('location: discussao.php?id='. $_GET['topid']);
 ?>
