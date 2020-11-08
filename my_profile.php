27b5f6e47db60d4750aca768f2ab1db1d40ae7ea
Everthonh <everthon.henrique.75@gmail.com>
2020-11-08 17:53:37 -0300

    Redirecionamento para a pagina de editar perfil

--- my_profile.php
+++ my_profile.php
@@ -44,7 +44,7 @@ $stmt = $pdo->prepare("
                       <h4><p class="text-primary mb-1"><?= $consulta[0]['US_DESCRIPTION'] ?></p></h4>
                       <?php if ($_SESSION['login'] == $_GET['id']): ?>
 
-                      <p><a href="profile.php">Editar perfil</a></p>
+                      <p><a href="editprofile.php">Editar perfil</a></p>
                       <?php endif ?>
                       
                     </div>
@@ -128,48 +128,61 @@ $stmt = $pdo->prepare("
 
         <?php if($consulta[$i]['TOP_TITLE'] == null) continue ?>
               
-                    <div class="container mb-3 shadow border border-light rounded">
-
-        <div class="row justify-content-center">
-          <div class="col-2 pt-3">
-
-            <?php if ($consulta[$i]['US_IMAGE'] == null || $consulta[$i]['US_IMAGE'] == 'upload/'): ?>
-                      <svg width="3em" height="3em" viewBox="0 0 20 20" class="bi bi-person-circle d-block m-auto" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
-                <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                <path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
-            </svg>
-                    <?php else: ?>
-                        <img class="img-topics rounded d-block m-auto" src="<?= $consulta[$i]['US_IMAGE'] ?>">
-             <?php endif ?>
-          </div>
-          <div class="col-5 text-left pt-3"><strong><?=$consulta[$i]['TOP_TITLE'] ?></strong>
-          </div>
-          <div class="col-3 text-left pt-3">Criado <time class="timeago" datetime="<?= date('Y-m-d H:m:s', strtotime($consulta[$i]['TOP_DATE'])) ?>"></time></div>
-          <div class="col-2">
-            <a href="#">
-              <svg width="3em" height="3em" viewBox="0 0 20 20" class="bi bi-trash-fill d-block m-auto pt-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
-            </svg>
-          </a>
-          </div>
+      <div class="container mb-3 shadow border border-light rounded">
+            <div class="row justify-content-center">
+              <div class="col-2 pt-3">
+                <?php if ($consulta[$i]['US_IMAGE'] == null || $consulta[$i]['US_IMAGE'] == 'upload/' . $_SESSION['login']): ?>
+                    <svg width="3em" height="3em" viewBox="0 0 20 20" class="bi bi-person-circle d-block m-auto" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
+                      <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                      <path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
+                </svg>
+                        <?php else: ?>
+                          <img class="img-topics rounded-circle d-block m-auto" src="<?= $consulta[$i]['US_IMAGE'] ?>">
+                    <?php endif ?>
+              </div>
+              <div class="col-5 text-left pt-3">
+                <strong><?= $consulta[$i]['TOP_TITLE'] ?></strong>
+              </div>
+              <div class="col-3 text-right pt-3">Criado <time class="timeago" datetime="<?= date('Y-m-d H:i:s', strtotime($consulta[$i]['TOP_DATE'])) ?>"></time>
+              </div>
+                <?php if (isset($_SESSION['login']) && $consulta[$i]['TOP_US_ID'] == $_SESSION['login']): ?>
+                  <div class="col-2">
+                    <a class="delete" href="delete.php?id=<?= $consulta[$i]['TOP_ID'] ?>">
+                      <svg width="3em" height="3em" viewBox="0 0 20 20" class="bi bi-trash-fill d-block m-auto pt-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+                    </svg>
+                  </a>
+                  </div>
+                <?php else: ?>
+                  <div class="col-2"></div>
+                <?php endif ?>
+            </div>
+            <div class="row justify-content-center">
+              <div class="col-2 text-center ml-n2">Criado por: <?= $consulta[$i]['US_NAME'] ?></div>
+              <div class="col-10"></div>
+            </div>
+            <div class="row justify-content-center pb-3">
+              <div class="col-2"></div>
+              <div class="col-8 text-center">
+                <a class="a-topics" href="discussao.php?id=<?=$consulta[$i]['TOP_ID'] ?>">
+                  <strong>Clique aqui para visualizar toda a discussão!</strong>
+                </a>
+              </div>
+              <div class="col-2"></div>
+            </div>
         </div>
-        <div class="row justify-content-center">
-          <div class="col-2 text-center ml-n2">Criado por: <?= $consulta[$i]['US_NAME'] ?></div>
-          <div class="col-10"></div>
-        </div>
-        <div class="row justify-content-center pb-3">
-          <div class="col-10"></div>
-          <div class="col-2 text-center"><a class="a-topics" href="discussao.php?id=<?=$consulta[$i]['TOP_ID'] ?>"><strong>Ver mais...</strong></a></div>
-        </div>
-
-    </div>
 
             </div>
           </div>
         </div>
     </div>
     <?php endfor ?>
+    <script>
+        $('.delete').on('click', evt => {
+          if (!confirm("Quer realmente apagar o seu Tópico?")) evt.preventDefault()
+        })
+      </script>
 
     <?php include 'templates/footer.php' ?>
 
