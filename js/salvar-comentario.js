e52628e2c1e3df822fa969747a910a654a26f830
Huanderson Lima <mattewlima16@gmail.com>
2020-11-09 15:38:21 -0300

    remoção de linha de comentário desnecessário

--- js/salvar-comentario.js
+++ js/salvar-comentario.js
@@ -4,19 +4,21 @@ $('#form-comment').submit(function (e) {
     var us = $('#id_us').val();
     var post = $('#id_post').val();
     var comment = $('#comentario').val();
-
+    var img = $('#img').val();
     //console.log(us, post, comment);
 
     $.ajax({
         url:'comentario.php',
         method: 'POST',
-        data: {id_us: us, id_post:post, comentario: comment},
+        data: {id_us: us, id_post:post, comentario: comment, img:img},
         dataType: 'json'
     }).done(function (result) {
         console.log(result);
     })
 
 });
+
+
 
 
 
